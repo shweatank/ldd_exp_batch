4743754a2275f2861cd97e978d55bb83526ee77c
devashreekatarkar <devashreekatarkar@mirafra.com>
2025-11-11 18:54:10 +0530

    Day 2 Implemented & Understood gdb (userspace) static and dynamic linking C concepts brush up
    Signed-off-by: devashreekatarkar <devashreekatarkar@mirafra.com>

---
+++ day2/static_linking/mathlib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void pr_sqr(int n){
+	printf("The area of square with side %d : %d\n",n,n*n);
+	return;
+}
---
+++ day2/static_linking/new_main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	pr_sqr(5);
+	return 0;
+}
