985f6aedab7b4f372bd0dbb02927c7158e50554e
ksv <saivarshath@mail.mirafra.com>
2025-11-13 09:02:55 +0530

    code

--- Static_linking/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "math.h"
-
-int main()
-{
-    print_square(5);
-    return 0;
-}
-
--- dynamic_linking/mathlib.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-#include "math.h"
-void print_square(int n)
-{
-    printf("Square of %d:-- %d\n",n,n*n);
-}
